1a848bb35881b6413b9bfcc52e053286679e2d63
mostafij101 <p.mostafij@gmail.com>
2016-06-04 07:28:29 -0500

    App is complete

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Properties
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    //Outlets
     @IBOutlet weak var logoImg: UIImageView!
     @IBOutlet weak var howManyTapsTxt: UITextField!
     @IBOutlet weak var playBtn: UIButton!
@@ -17,13 +21,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var coinBtn: UIButton!
     @IBOutlet weak var tapsLabel: UILabel!
     
-    @IBAction func playBtnPressed(sender: UIButton!){
-        logoImg.hidden = true
-        howManyTapsTxt.hidden = true
-        playBtn.hidden = true
+    @IBAction func onCoinBtnTapped(sender: UIButton!){
+        currentTaps += 1
+        updateCurrentTaps()
         
-        coinBtn.hidden = false
-        tapsLabel.hidden = false
+        if isGameOver(){
+            restartGame()
+        }
+    }
+    
+    @IBAction func playBtnPressed(sender: UIButton!){
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != ""{
+            logoImg.hidden = true
+            howManyTapsTxt.hidden = true
+            playBtn.hidden = true
+            
+            coinBtn.hidden = false
+            tapsLabel.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            updateCurrentTaps()
+        }
+    }
+    
+    func restartGame(){
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        logoImg.hidden = false
+        howManyTapsTxt.hidden = false
+        playBtn.hidden = false
+        
+        coinBtn.hidden = true
+        tapsLabel.hidden = true
+    }
+    
+    func isGameOver() -> Bool{
+        if currentTaps >= maxTaps{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func updateCurrentTaps(){
+        tapsLabel.text = "\(currentTaps) taps"
     }
 
 }
